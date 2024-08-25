4022c37e7505bdc189fcff0f7c89e66b9b1d2e54
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-08-26 04:21:31 +0900

    2024.08.26 Programmers Lv0

---
+++ js181846.js
@@ -0,0 +1,6 @@
+// const solution = (a, b) => BigInt(a)+BigInt(b)+"";
+const solution = (a, b) => eval(a+"n") + eval(b+"n") + "";
+
+
+console.log(solution("582",	"734"));
+console.log(solution("18446744073709551615","287346502836570928366"));
