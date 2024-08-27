807ab893c372a316216907de124c2cb9e6a76bba
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-08-28 00:52:41 +0900

    181832 프로그래머스 완료 2024.08.28

--- js181832.js
+++ js181832.js
@@ -1,22 +1,47 @@
 const solution = (n) => {
-    let maxRow = n;
-    let row = 0;
-    let maxCol = n;
-    let col = 0;
-    let numbers = Array.from({length:n**2},(_,index)=>index+1);
-    let result = Array.from({length:n},()=>Array(n).fill(0));
-    numbers.forEach((number)=>{
-        if(number<=maxRow) {
-            result[col][row] = number;
-            row++;
+  let maxRow = n - 1;
+  let minRow = 0;
+  let row = 0;
+  let maxCol = 0;
+  let minCol = 0;
+  let col = 0;
+  let revers = false;
+  let numbers = Array.from({ length: n ** 2 }, (_, index) => index + 1);
+  let result = Array.from({ length: n }, () => Array(n).fill(0));
+  numbers.forEach((number) => {
+    if (!revers) {
+      if (row <= maxRow && col <= maxCol) {
+        result[col][row] = number;
+        row <= maxRow ? row++ : row;
+        if (row > maxRow) {
+          maxCol = maxRow;
+          row--;
+          col <= maxCol ? col++ : col;
         }
-
-        if(row>maxRow&&col>maxCol){
-            
+        if (row == maxRow && col > maxCol) {
+          revers = !revers;
+          col-- && maxRow-- && minCol++;
+          row = --row;
         }
-    })
-    return result;
-}
+      }
+    } else {
+      if (row >= minRow && col >= minCol) {
+        result[col][row] = number;
+        row >= minRow ? --row : row;
+        if (row < minRow) {
+          row++;
+          col >= minCol ? col-- : col;
+        }
+        if (row == minRow && col < minCol) {
+          revers = !revers;
+          col++ && maxCol--;
+          minRow = ++minRow;
+          row = ++row;
+        }
+      }
+    }
+  });
+  return result;
+};
 
-
-console.log(solution(5));
+console.log(solution(4));
