11cfdbaf0bfdab429c709751ba156c64c7b7f55d
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-09-03 17:31:20 +0900

    2024.09.03 Programmers Lv0

--- js120923.js
+++ js120923.js
@@ -1,20 +1,26 @@
 const solution = (num, total) => {
-    return check(Array(num).fill(0).map((_,index)=>total-index).reverse());
+  return check(
+    Array(num)
+      .fill(0)
+      .map((_, index) => 1000 - index)
+      .reverse()
+  );
 
-    function check(list){
-        if(list.reduce((sum,num)=>sum+num,0)==total){
-            return list;
-        } else {
-            return check(list.map(num=>num-1));
-        }
+  function check(list) {
+    if (list.reduce((sum, num) => sum + num, 0) == total) {
+      return list;
+    } else {
+      return check(list.map((num) => num - 1));
     }
-}
-console.log(solution(3,12));
-console.log(solution(5,5));
-console.log(solution(1,0));
-console.log(solution(4,14));
-console.log(solution(5,15));
-console.log(solution(5,15));
-console.log(solution(6,21));
-console.log(solution(1,1));
-console.log(solution(1,2));
+  }
+};
+console.log(solution(3, 12));
+console.log(solution(5, 5));
+console.log(solution(1, 0));
+console.log(solution(4, 14));
+console.log(solution(5, 15));
+console.log(solution(5, 15));
+console.log(solution(6, 21));
+console.log(solution(1, 1));
+console.log(solution(1, 2));
+console.log(solution(5, 0));
