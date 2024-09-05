dcce9dacae6741119773baacf76129a370835d65
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-09-06 01:47:40 +0900

    24.09.06 Programmers Lv0
    문자열 계산하기.Re
    
    ⚠️ eval 함수 사용 제외
    🔖 split( ), join( ), match( ), reduce( ) 활용
    ✏️ split( ), join( ) 으로 빈 문자열 제거.
    ✏️ match(/ \W*\d+/g)를 구분으로 각 숫자 배열로 분리
    ✏️ 분리된 숫자를 reduce( )로 계산.

--- js120902.js
+++ js120902.js
@@ -1,3 +1,7 @@
-const solution = (my_string) => eval(my_string);
+// ⚠️ eval 함수는 사용하지 않는 편이 좋다.
 
-console.log(solution("3 + 4"))
+// const solution = (my_string) => eval(my_string);
+
+const solution = (my_string) => my_string.split(" ").join("").match(/\W*[\d]+/g).reduce((result,num)=>result+= +num,0);
+
+console.log(solution("13 + 1 + 4"))
