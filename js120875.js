a8f50dac1cce564f496f05c67fe3206d818439c6
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-09-10 17:23:47 +0900

    24.09.08 Programmers Lv0 - 평행 (완료)
    🔖 reduce와 filter, Math abs 활용
    
    ✏️ 좌표 평행하는 공식
    ( X1 - X2 ) / ( Y1 - Y2 ) = ( X3 - X4 ) / ( Y3 - Y4 )

--- js120875.js
+++ js120875.js
@@ -1,12 +1,16 @@
-const solution = ([a,b,c,d]) => a.reduce((result,_,index)=>
-    (a[index]-b[index])-(c[index]-d[index])==0?1:result,0);
+const solution = (dots) => {
+    return dots.reduce((result,arr,index,original)=>{
+        let [a,b,c,d] = [arr,...(original.filter((_,filterIndex)=>filterIndex!=index))];
+        return Math.abs(a[0]-b[0])/Math.abs(a[1]-b[1])==Math.abs(c[0]-d[0])/Math.abs(c[1]-d[1])?1:result
+    },0)
+}
 
-console.log(solution([[2, 4], [9, 2], [3, 8], [11, 6]]));
+
+
+
+
+
+console.log(solution([[1, 4], [9, 2], [3, 8], [11, 6]]));
 console.log(solution([[3, 5], [4, 1], [2, 4], [5, 10]]));
 console.log(solution([[1, 2], [5, 1], [3, 6], [6, 3]]));
-
-console.log((2-9)-(3-11))
-console.log((2-9))
-console.log((3-11))
-// 2, 9, 3, 11
-// 4, 2, 8, 6
+console.log(solution([[1, 1], [1, 2], [3, 3], [4, 1]]));
