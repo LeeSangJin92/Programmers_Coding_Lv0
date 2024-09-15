06b9e21b8949c635a697cdcd16c88d64bccf6f50
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-09-15 18:04:08 +0900

    24 .09 .15 Programmers Lv0 - 문자열 뒤집기
    🔖 얕은 복사 / reverse() / join 활용
    
    ✏️ 문자열을 배열로 분리 후 reverse() 로 뒤집은 후 join 조합
    ⚠️ 20823은 문제 이해가 안되어 추후 도전

---
+++ js120822.js
@@ -0,0 +1,3 @@
+const solution = (my_string) => [...my_string].reverse().join('');
+
+console.log(solution("jaron"));
---
+++ js120823.js
@@ -0,0 +1,19 @@
+// 문제 이해 부족.. 나중에 시도 예정
+
+// const solution = (n) => Array(n).fill().map((_,index)=>"*".repeat(index+1)).join('\n');
+
+// console.log(solution(3));
+
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// let input = [];
+
+// rl.on('line', function (line) {
+//     input = line.split(' ');
+// }).on('close', function () {
+//     console.log(Number(input[0]));
+// });
