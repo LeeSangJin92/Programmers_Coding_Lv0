6161a81e73d06bf4e988aaa984b7994d28781b8a
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-09-18 21:12:15 +0900

    24 .09 .16 Programmers Lv0 - 분수의 덧셈
    🔖 최소 공배수 공식 활용
    
    ✏️ 두 분수의 분모들을 매개변수로 최소 공배수를 구함.
    ✏️ 최소 공배수로 분자들 계산
    ✏️ 이후 나온 분모와 분자 사이에 공배수를 다시 계산
    ✏️ 나온 공배수로 분자와 분모를 나누어 기약분수로 만듬

--- js120808.js
+++ js120808.js
@@ -2,12 +2,13 @@ const solution = (numer1, denom1, numer2, denom2) => {
     let lcm = denom1 * denom2 / gcd(denom1, denom2);
     let num1 = lcm/denom1;
     let num2 = lcm/denom2;
-    console.log(num1, num2);
-
-    return [numer1*num1+numer2*num2, lcm];
+    let numerator = numer1*num1+numer2*num2;
+    let answerGcd = gcd(numerator,lcm);
+    return [numerator/answerGcd, lcm/answerGcd]; 
 }
 
 const gcd = (num1, num2) => num2 == 0 ? num1 : gcd(num2, num1 % num2);
 
 console.log(solution(1,2,3,4))
 console.log(solution(9,2,1,3))
+console.log(solution(2, 2, 2, 2))
