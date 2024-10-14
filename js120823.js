0d4f60e9913ae0e96251775dd96da786750806af
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-10-15 02:04:03 +0900

    24 .10 .15 Programmers Lv0 - 직각삼각형 출력하기 (⭐Node)
    ⭐ Node 활용해서 테스트해야하는 문제
    
    🔖 while / repeat 활용
    ✏️ 주워진 정수만큼 while문을 구동
    ✏️ while문이 실행 될떄 마다 별에 repeat만큼 출력

--- js120823.js
+++ js120823.js
@@ -1,19 +1,19 @@
-// 문제 이해 부족.. 나중에 시도 예정
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-// const solution = (n) => Array(n).fill().map((_,index)=>"*".repeat(index+1)).join('\n');
+let input = [];
+rl.on('line', function (line) {
+    input = line.split(' ');
+    let [i, number] = [1,input[0]];
+    while(i<=number){
+        console.log("*".repeat(i));
+        ++i;
+    }
+}).on('close', function () {
+});
 
-// console.log(solution(3));
 
-// const readline = require('readline');
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
 
-// let input = [];
-
-// rl.on('line', function (line) {
-//     input = line.split(' ');
-// }).on('close', function () {
-//     console.log(Number(input[0]));
-// });
